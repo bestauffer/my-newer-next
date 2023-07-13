90af2487d8a4719ac314947119883b5e1e5d7ea5
bestauffer <77029920+bestauffer@users.noreply.github.com>
2023-07-13 08:58:54 -0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,9 @@
 import '@@/styles/globals.css';
-import Image from 'next/image'
+import Image from 'next/image';
+import Script from 'next/script';
 export default function Page() {
   return (
     <>
-    <head>
-    <script src="https://platform.linkedin.com/badges/js/profile.js" async defer type="text/javascript"></script>
-    </head>
     <div style={{backgroundColor:"white", padding:"20px 0px 20px 0px"}}class=" justify-content-center d-flex flex-row">
     <div class=" align-items-center justify-content-center d-flex flex-column">
     
@@ -13,14 +11,12 @@ export default function Page() {
       {/* <a class="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/blake-stauffer-174163156?trk=profile-badge">Blake S.</a> */}
       </div>
       <div>
-        
           <a rel="noopener noreferrer" href="https://github.com/bestauffer"><Image unsized alt="Github Banner" src='https://img.shields.io/badge/GitHub-181717.svg?style=for-the-badge&logo=GitHub&logoColor=white'/></a>
-        
-      
         </div>        
               
     </div>
     </div>
+    <Script src="https://platform.linkedin.com/badges/js/profile.js"/>
     </>
   );
 }
