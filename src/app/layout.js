85091c227339c6c0e5e0ce3974e2b344fd844988
bestauffer <77029920+bestauffer@users.noreply.github.com>
2023-07-17 18:32:58 -0700

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,7 @@ require("bootstrap/dist/js/bootstrap");
 
   return (
     <>
-    <Script src="https://platform.linkedin.com/badges/js/profile.js"/>
+    
     <html lang="en">
       <head>
         <title>Blakes Next.js</title>
@@ -68,7 +68,7 @@ require("bootstrap/dist/js/bootstrap");
           {children}
         </div>
         </div>
-        
+        <Script src="https://platform.linkedin.com/badges/js/profile.js"/>
       </body>
     </html>
     </>
