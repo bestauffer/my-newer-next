8e50c5a5260086c35634ad59ab487004664eaa45
bestauffer <77029920+bestauffer@users.noreply.github.com>
2023-07-17 18:16:45 -0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import Script from 'next/script';
 export default function Page() {
   return (
     <>
+    <Script src="https://platform.linkedin.com/badges/js/profile.js" strategy="lazyOnload" />
     <div style={{backgroundColor:"white", padding:"20px 0px 20px 0px"}}class=" justify-content-center d-flex flex-row">
     <div class=" align-items-center justify-content-center d-flex flex-column">
     
@@ -12,11 +13,10 @@ export default function Page() {
       </div>
       <div>
           <a rel="noopener noreferrer" href="https://github.com/bestauffer"><Image unsized alt="Github Banner" src='https://img.shields.io/badge/GitHub-181717.svg?style=for-the-badge&logo=GitHub&logoColor=white'/></a>
-        </div>        
-              
+        </div>             
     </div>
     </div>
-    <Script src="https://platform.linkedin.com/badges/js/profile.js" strategy="lazyOnload" />
+    
     </>
   );
 }
