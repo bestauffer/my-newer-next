27747674ce95623c03cf35eb84afe414e0853061
bestauffer <77029920+bestauffer@users.noreply.github.com>
2023-07-17 20:31:39 -0700

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -10,7 +10,7 @@ export default function Page() {
     <div class=" align-items-center justify-content-center d-flex flex-column">
     
     <div class="LI-profile-badge" data-locale="en_US" data-size="large" data-theme="dark" data-type="HORIZONTAL" data-vanity="blake-stauffer-174163156" data-version="v1">
-      <a style={{color:"black" }} class="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/blake-stauffer-174163156?trk=profile-badge">Blake S.</a>
+      <a style={{color:"black" }} class="badge-base__link LI-simple-link" href="https://www.linkedin.com/in/blake-stauffer-174163156?trk=profile-badge">Click Here to Connect With my LinkedIn</a>
       </div>
       <div>
           <a rel="noopener noreferrer" href="https://github.com/bestauffer"><Image unsized alt="Github Banner" src='https://img.shields.io/badge/GitHub-181717.svg?style=for-the-badge&logo=GitHub&logoColor=white'/></a>
